0022bb796ce907fb82eb15709e6a28fa3d470832
rjwang <r.jiewang@gmail.com>
2015-06-05 10:58:11 +0200

    update dataset

--- RPCInclusionAnalyzer/test/rpc_inclusion_cfg.py
+++ RPCInclusionAnalyzer/test/rpc_inclusion_cfg.py
@@ -6,18 +6,18 @@ from L1TCSCTFUpgrade.RPCInclusionAnalyzer.rpc_inclusion_cfi import *
 
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True),
                                         SkipEvent = cms.untracked.vstring('ProductNotFound')
                                         )
 
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-	'/store/data/Run2015A/Commissioning/RAW/v1/000/246/926/00000/6A496D1A-F709-E511-B2B9-02163E0145D2.root',
+	'/store/data/Run2012D/SingleMu/RAW-RECO/ZMu-22Jan2013-v1/10000/1010C4E3-A1A7-E211-B288-00259074AE9A.root',
     )
 )
 
